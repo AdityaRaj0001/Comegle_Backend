cad0319a63414912c44a51122ce56a031ebcb941
meadityaraj <raj.aditiya@skdiv.com>
2025-09-14 23:15:04 +0530

    fix: update CORS origin to correct frontend URL

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,6 +2,6 @@ import cors from "cors";
 import { CLIENT_URL } from "./env";
 
 export const corsOptions = cors({
-  origin: [CLIENT_URL, "https://comegle-client.vercel.app","https://www.comegle.live","https://comegle.live"], // whitelist dev + prod
+  origin: [CLIENT_URL, "https://comegle-frontend.vercel.app","https://www.comegle.live","https://comegle.live"], // whitelist dev + prod
   credentials: true,
 });
