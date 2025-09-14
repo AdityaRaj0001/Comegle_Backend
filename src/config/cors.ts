7a56074406e38659bf9631cbdfaa6ed18ec3c427
AdityaRaj0001 <meadityaraj0001@gmail.com>
2025-09-14 23:21:21 +0530

    fix: remove CLIENT_URL from CORS origin whitelist

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,7 +1,6 @@
 import cors from "cors";
-import { CLIENT_URL } from "./env";
 
 export const corsOptions = cors({
-  origin: [CLIENT_URL, "https://comegle-frontend.vercel.app","https://www.comegle.live","https://comegle.live"], // whitelist dev + prod
+  origin: ["https://comegle-frontend.vercel.app","https://www.comegle.live","https://comegle.live"], // whitelist dev + prod
   credentials: true,
 });
