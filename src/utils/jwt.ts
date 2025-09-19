d2445dafee42d403537a829db58c22d8642808dd
Aman Raj <94280987+IAMAmanRaj@users.noreply.github.com>
2025-09-19 09:54:05 +0530

    Change access Token expiry time

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -4,8 +4,8 @@ const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET!;
 const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET!;
 
 export function generateAccessToken(payload: object) {
-  // return jwt.sign(payload, ACCESS_TOKEN_SECRET, { expiresIn: "10m" });
-  return jwt.sign(payload, ACCESS_TOKEN_SECRET, { expiresIn: "15m" });
+  // return jwt.sign(payload, ACCESS_TOKEN_SECRET, { expiresIn: "15seconds" });
+  return jwt.sign(payload, ACCESS_TOKEN_SECRET, { expiresIn: "2m" }); // 10 minutes
 }
 export function generateRefreshToken(payload: object) {
   // return jwt.sign(payload, REFRESH_TOKEN_SECRET, { expiresIn: "7d" });
