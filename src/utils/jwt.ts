5eebab52cc7648a7ed828d1f09025caf710f07b0
AdityaRaj0001 <meadityaraj0001@gmail.com>
2025-09-19 18:01:00 +0530

    feat: update access token expiry time to 30 minutes

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -5,7 +5,7 @@ const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET!;
 
 export function generateAccessToken(payload: object) {
   // return jwt.sign(payload, ACCESS_TOKEN_SECRET, { expiresIn: "15seconds" });
-  return jwt.sign(payload, ACCESS_TOKEN_SECRET, { expiresIn: "2m" }); // 10 minutes
+  return jwt.sign(payload, ACCESS_TOKEN_SECRET, { expiresIn: "30m" }); // 30 minutes
 }
 export function generateRefreshToken(payload: object) {
   // return jwt.sign(payload, REFRESH_TOKEN_SECRET, { expiresIn: "7d" });
