08dede0f704f27ce9ddae97f1124fd11819db360
Aman Raj <imamanraj87@gmail.com>
2025-09-25 10:38:17 +0530

    feat: add community waitlist functionality with controller and routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import healthRoutes from "./health.routes";
 import onboardingRoutes from "./onboarding.routes";
 import userRoutes from "./user.routes";
 import collegeRoutes from "./college.route";
+import communityRoutes from "./communities.routes";
 const router = Router();
 
 router.use("/auth", authRoutes); // <-- Added here
@@ -11,5 +12,6 @@ router.use("/onboarding", onboardingRoutes);
 router.use("/health", healthRoutes);
 router.use("/user", userRoutes); // <-- Added here
 router.use("/college", collegeRoutes); // <-- Added here
+router.use("/communities",communityRoutes)
 
 export default router;
