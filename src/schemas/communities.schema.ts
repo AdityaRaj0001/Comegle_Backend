3a6dc13c615fbb846b33bec76639c5adcbd55757
Aman Raj <raj.aman@skdiv.com>
2025-09-25 12:44:13 +0530

    refactor: add validation schema for community waitlist request

---
+++ src/schemas/communities.schema.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const addCommunityToWaitListSchema = z.object({
+  community_name : z
+    .string()
+    .min(1, "Community name cannot be empty")
+    .max(30, "Community name should must be below 30 characters"),
+});
